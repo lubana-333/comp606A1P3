a221b45ea83362383e6e8b066e9b9605121726d3
lubana-333 <52937205+lubana-333@users.noreply.github.com>
2019-09-19 20:37:29 +1200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,227 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">	
+		
+		<title>Therapy</title>
+
+		<meta name="description" content="">
+		<meta name="keywords" content="" />
+		<meta name="author" content="">
+
+	<?php include "lib/top.inc.php" ?>
+
+</head>
+
+<body>
+
+<?php include "lib/header.inc.php" ?>
+
+<!-- Showcase Section Starts here -->
+<section class="slider">
+		<div id="wowslider-container1">
+			<div class="ws_images">
+
+				<ul>
+					<li>	
+						<a href="#">
+							<img src="img/sider-2.jpg" id="wows1_0"/>
+							
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>	
+</section>
+
+<section class="home_hightlight">
+	
+	<div class="container">
+		<div class="row">			
+			
+			<div class="col-sm-4 text-center">
+				<span><i class="fa fa-hospital-o" aria-hidden="true"></i></span>
+				<h3>FOR CLIENT</h3>
+				<p>Awesome support and hugely beneficial to optimise performance and stay injury free.We can make a massage longer or shorter as you wish, 30 to 120 minutes. We can mix and match our massage techniques to your liking. </p>
+								<a href="#">Read More</a>
+							</div>
+			
+			<div class="col-sm-4 text-center">
+				<span><i class="fa fa-graduation-cap" aria-hidden="true"></i></span>
+				<h3>Our Experts</h3>
+				<p>We have best massage experts who have the great massage skill and techniques.</p>
+								<a href="#">Read More</a>
+							</div>
+			
+			<div class="col-sm-4 text-center">
+				<span class="hvr-fade"><i class="fa fa-user-md" aria-hidden="true"></i></span>
+				<h3>Massage Type</h3>
+				<p>We have several masaage type like, Deep tissue massage, Sports masssage, Therapeutic Massage, Relaxation Massage</p>
+								<a href="#">Read More</a>
+							</div>
+		</div> <!-- row ends here -->
+	</div> <!-- Container ends here -->
+</section>
+
+<section id="testimonials_section">
+	<div class="container">
+		<div class="row">	
+			<div class="col-sm-12">
+				<h1>Client Testimonials</h1>
+				<div id="cl-testimonials" class="owl-carousel owl-theme owl-loaded">              
+
+		
+			<div class="owl-stage-outer">
+				<div class="owl-stage" style="transform: translate3d(-2300px, 0px, 0px); transition: all 0s ease 0s; width: 6900px;">
+					<div class="owl-item cloned" style="width: 1140px; margin-right: 10px;">
+						<div class="item">
+							<div class="cl-speak">
+								<em>
+								   <p>
+									<span style="font-family: Consolas, &quot;Lucida Console&quot;, monospace; white-space: pre-wrap;">
+										Duis non condimentum nunc. Nunc quis turpis eu est tincidunt rutrum. Cras a purus quis sem tincidunt egestas vel id lacus. Nunc sed volutpat erat. Duis sollicitudin ac nisi eget rutrum. Nunc sagittis velit nec sapien blandit molestie.
+									</span>
+								  </p>
+								</em>
+					
+					<div class="authorInfo">
+						<img src="img/5646_cl_bg.png" alt="MELANI GRIFFITH">
+							<p>
+							<span>MELANI GRIFFITH</span>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="owl-item cloned" style="width: 1140px; margin-right: 10px;"><div class="item">
+				<div class="cl-speak">
+					<em>
+						<p>
+						<span style="font-family: Consolas, &quot;Lucida Console&quot;, monospace; white-space: pre-wrap;">
+							Duis non condimentum nunc. Nunc quis turpis eu est tincidunt rutrum. Cras a purus quis sem tincidunt egestas vel id lacus. Nunc sed volutpat erat. Duis sollicitudin ac nisi eget rutrum.
+						</span>
+						</p>
+					</em>
+					
+					<div class="authorInfo">
+						<img src="img/29606_cl_bg.png" alt="MELANI GRIFFITH ">
+						<p>
+						<span>MELANI GRIFFITH </span>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+				<div class="owl-item active" style="width: 1140px; margin-right: 10px;"><div class="item">
+			
+				<div class="cl-speak">
+					<em>
+					<p>
+					
+					<span style="font-family: Consolas, &quot;Lucida Console&quot;, monospace; white-space: pre-wrap;">Duis non condimentum nunc. Nunc quis turpis eu est tincidunt rutrum. Cras a purus quis sem tincidunt egestas vel id lacus. Nunc sed volutpat erat. Duis sollicitudin ac nisi eget rutrum. Nunc sagittis velit nec sapien blandit molestie.</span></p>
+					
+					</em>
+						<div class="authorInfo">
+						
+							<img src="img/5646_cl_bg.png" alt="MELANI GRIFFITH">
+						
+								<p>
+									<span>MELANI GRIFFITH</span>
+									
+								</p>
+						</div>
+				</div>
+			</div>
+		</div>
+			
+			<div class="owl-item" style="width: 1140px; margin-right: 10px;">
+			
+				<div class="item">
+					
+						<div class="cl-speak">
+					
+					    <em>
+							<p>
+								<span style="font-family: Consolas, &quot;Lucida Console&quot;, monospace; white-space: pre-wrap;">Duis non condimentum nunc. Nunc quis turpis eu est tincidunt rutrum. Cras a purus quis sem tincidunt egestas vel id lacus. Nunc sed volutpat erat. Duis sollicitudin ac nisi eget rutrum.</span></p>
+						</em>
+
+					<div class="authorInfo">
+						
+						<img src="img/29606_cl_bg.png" alt="MELANI GRIFFITH ">
+						
+							<p>
+								<span>MELANI GRIFFITH </span>
+							</p>
+					</div>
+				</div>
+			</div>
+		</div>
+			
+		   <div class="owl-item cloned" style="width: 1140px; margin-right: 10px;"><div class="item">
+				<div class="cl-speak">
+					<em>
+						<p>
+						<span style="font-family: Consolas, &quot;Lucida Console&quot;, monospace; white-space: pre-wrap;">Duis non condimentum nunc. Nunc quis turpis eu est tincidunt rutrum. Cras a purus quis sem tincidunt egestas vel id lacus. Nunc sed volutpat erat. Duis sollicitudin ac nisi eget rutrum. Nunc sagittis velit nec sapien blandit molestie.</span>
+						
+						</p>
+					</em>
+
+						<div class="authorInfo">
+							<img src="img/5646_cl_bg.png" alt="MELANI GRIFFITH">
+							<p><span>MELANI GRIFFITH</span></p>
+						
+						</div>
+					</div>
+				</div>
+		    </div>
+			
+			<div class="owl-item cloned" style="width: 1140px; margin-right: 10px;">
+				<div class="item">
+					<div class="cl-speak">
+						<em>
+							<p>
+								<span style="font-family: Consolas, &quot;Lucida Console&quot;, monospace; white-space: pre-wrap;">Duis non condimentum nunc. Nunc quis turpis eu est tincidunt rutrum. Cras a purus quis sem tincidunt egestas vel id lacus. Nunc sed volutpat erat. Duis sollicitudin ac nisi eget rutrum.
+								</span>
+							</p>
+						</em>
+								<div class="authorInfo">
+									<img src="img/29606_cl_bg.png" alt="MELANI GRIFFITH ">
+								<p>
+									<span>MELANI GRIFFITH </span>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+			
+			
+			<div class="owl-controls"><div class="owl-nav">
+				<div class="owl-prev" style="display: none;">prev</div>
+					<div class="owl-next" style="display: none;">next</div>
+				
+			</div>
+							<div class="owl-dots" style="">
+								<div class="owl-dot active"><span></span>
+							</div>
+								<div class="owl-dot"><span></span>
+								</div>
+							</div>
+						</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include "lib/footer.inc.php" ?>
+<script type="text/javascript" src="js/wowslider.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+<link rel="stylesheet" type="text/css" href="css/slider.css" />
+</body>
+</html>
